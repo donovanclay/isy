5f6d66106e5b0f318037a547598d975257176cff
donovanclay <donovan@donovanclay.com>
2023-08-30 20:09:22 -0700

    clean up code

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-import os
-from dotenv import load_dotenv
 import asyncio
+import json
 import logging
+import os
 import time
 from urllib.parse import urlparse
-import json
-import tkinter as tk
-from tkinter import ttk
-from typing import Tuple 
 
-# local files
-from fan import Fan, ExhaustFan, ExhaustFans, SupplyFan, SupplyFans
-from color import color
-#
-
+from dotenv import load_dotenv
 from pyisy import ISY
 from pyisy.connection import ISYConnectionError, ISYInvalidAuthError, get_new_client_session
-from pyisy.constants import NODE_CHANGED_ACTIONS, SYSTEM_STATUS
-from pyisy.logging import LOG_VERBOSE, enable_logging
+from pyisy.constants import NODE_CHANGED_ACTIONS
+from pyisy.logging import enable_logging
 from pyisy.nodes import NodeChangedEvent
+
+from color import color
+# local files
+from fan import ExhaustFans, SupplyFans
 
 load_dotenv()
 
@@ -104,36 +100,30 @@ async def main(url, username, password, tls_ver, events, node_servers):
                 system_status_handler
             )
 
-
         # -----------------------------------------
-        # CLAYHUANG CODE STARTS HERE 
+        # CLAY HUANG CODE STARTS HERE
         # -----------------------------------------
-
 
         with open("util.json", "r") as file:
             file_data = json.load(file)
-        
-        exhaust_fansObject = ExhaustFans(isy, file_data.get("exhaust_fan_node_names"))
-        supply_fansObject = SupplyFans(isy, file_data.get("supply_fan_node_names"))
 
-        exhaust_fans = exhaust_fansObject.dict
-        supply_fans = supply_fansObject.dict
+        exhaust_fans_object = ExhaustFans(isy, file_data.get("exhaust_fan_node_names"))
+        supply_fans_object = SupplyFans(isy, file_data.get("supply_fan_node_names"))
 
         while True:
             # this is the period for the clock cycle of the program
             # without this the program would always be running at full speed
             await asyncio.sleep(1)
 
-            exhaust_fansObject.update()
-            supply_fansObject.update()
+            exhaust_fans_object.update()
+            supply_fans_object.update()
 
-            exhaust_fans = exhaust_fansObject.dict
-            supply_fans = supply_fansObject.dict
+            exhaust_fans = exhaust_fans_object.dict
+            supply_fans = supply_fans_object.dict
 
-
-            exhaust_cfm = await getExhaustCFM(exhaust_fans)
-            supply_cfm = await getSupplyCFM(supply_fans)
-            net_cfm = await balanceCFM(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm)
+            exhaust_cfm = await get_exhaust_cfm(exhaust_fans)
+            supply_cfm = await get_supply_cfm(supply_fans)
+            net_cfm = await balance_cfm(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm)
 
             for exhaust_fan in exhaust_fans:
                 fan = exhaust_fans[exhaust_fan]
@@ -142,7 +132,6 @@ async def main(url, username, password, tls_ver, events, node_servers):
             for supply_fan in supply_fans:
                 fan = supply_fans[supply_fan]
                 print(fan)
-                    
 
             print("{}TOTAL EXHAUST CFM: {}{}".format(color.BOLD, exhaust_cfm, color.END))
             print("{}TOTAL SUPPLY CFM: {}{}".format(color.BOLD, supply_cfm, color.END))
@@ -150,8 +139,6 @@ async def main(url, username, password, tls_ver, events, node_servers):
             print(color.BOLD + time.ctime(time.time()) + color.END)
             print("-----------------------------------------")
 
-              
-               
     except asyncio.CancelledError:
         pass
     finally:
@@ -161,38 +148,41 @@ async def main(url, username, password, tls_ver, events, node_servers):
             system_status_subscriber.unsubscribe()
         await isy.shutdown()
 
-# This method returns the total exhuast cfm of all the fans
-async def getExhaustCFM(exhaust_fans):
+
+# This method returns the total exhaust cfm of all the fans
+async def get_exhaust_cfm(exhaust_fans):
     cfm = 0
     for exhaust_fan in exhaust_fans:
         fan = exhaust_fans[exhaust_fan]
         # print(fan)
-        
+
         if fan.type == "bool":
             cfm += fan.value and fan.cfm
-        
+
         if type(fan.type) == int:
-            ratio = 1/fan.type
+            ratio = 1 / fan.type
             cfm += round(fan.cfm * fan.value * ratio)
 
     return cfm
 
-async def getSupplyCFM(supply_fans):
+
+async def get_supply_cfm(supply_fans):
     cfm = 0
     for supply_fan in supply_fans:
         fan = supply_fans[supply_fan]
         # print(fan)
-        
+
         if fan.type == "bool":
             cfm += fan.value and fan.cfm
-        
+
         if type(fan.type) == int:
-            ratio = 1/fan.type
+            ratio = 1 / fan.type
             cfm += round(fan.cfm * fan.value * ratio)
 
     return cfm
 
-async def getFan(exhaust_fans, supply_fans, node_name):
+
+async def get_fan(exhaust_fans, supply_fans, node_name):
     for fan in exhaust_fans:
         if fan == node_name:
             return exhaust_fans[fan]
@@ -200,37 +190,41 @@ async def getFan(exhaust_fans, supply_fans, node_name):
         if fan == node_name:
             return supply_fans[fan]
 
-def getSupplyFanISYNode(supply_fans, index):
-    with open("util.json", "r") as file:
-        file_data = json.load(file)
-    node_name = file_data["supply_fan_node_names"][index]
-    return supply_fans[node_name].node
 
-async def turnOffSupplies(damper, fan_12_inch, fan_8_inch):
+# def get_supply_fan_ISY_node(supply_fans, index):
+#     with open("util.json", "r") as file:
+#         file_data = json.load(file)
+#     node_name = file_data["supply_fan_node_names"][index]
+#     return supply_fans[node_name].node
+
+
+async def turn_off_supplies(damper, fan_12_inch, fan_8_inch):
     await damper.node.turn_off()
     await fan_12_inch.node.turn_off()
     await fan_8_inch.node.turn_off()
 
+
 # returns the CFM the supply fan is set to
-async def turnOnSupply(fan, net_cfm):
+async def turn_on_supply(fan, net_cfm):
     # print("fan status: " + str(fan.node.status))
-    fan_percentage = min(1, net_cfm/fan.cfm)
+    fan_percentage = min(1, net_cfm / fan.cfm)
     # print("fan percentage: " + str(fan_percentage))
     cfm_of_fan = round(fan_percentage * fan.cfm)
-    print("Turning on fan for {} cfm".format(cfm_of_fan)) 
+    print("Turning on fan for {} cfm".format(cfm_of_fan))
     on_level = round(fan_percentage * 255)
     fan.value = on_level
     await fan.node.turn_on(int(on_level))
     return cfm_of_fan
-        
-async def balanceCFM(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm):
+
+
+async def balance_cfm(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm):
     FRESH_AIR = "n001_output_33"
     FRESH_AIR_FAN_12_INCH = "53 23 84 1"
     FRESH_AIR_FAN_8_INCH = "53 25 DA 1"
 
-    damper = await getFan(exhaust_fans, supply_fans, FRESH_AIR)
-    fan_12_inch = await getFan(exhaust_fans, supply_fans, FRESH_AIR_FAN_12_INCH)
-    fan_8_inch = await getFan(exhaust_fans, supply_fans, FRESH_AIR_FAN_8_INCH)
+    damper = await get_fan(exhaust_fans, supply_fans, FRESH_AIR)
+    fan_12_inch = await get_fan(exhaust_fans, supply_fans, FRESH_AIR_FAN_12_INCH)
+    fan_8_inch = await get_fan(exhaust_fans, supply_fans, FRESH_AIR_FAN_8_INCH)
 
     net_cfm = exhaust_cfm
 
@@ -242,8 +236,7 @@ async def balanceCFM(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm):
             await damper.node.turn_on()
             print("status after: " + str(damper.node.status))
 
-
-            damper.time_off = time.time() 
+            damper.time_off = time.time()
         net_cfm -= damper.cfm
 
         # if more supply is needed...
@@ -252,14 +245,14 @@ async def balanceCFM(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm):
             # if the damper is fully open
             if time.time() - damper.time_off > 33:
 
-                net_cfm -= await turnOnSupply(fan_8_inch, net_cfm)
+                net_cfm -= await turn_on_supply(fan_8_inch, net_cfm)
 
-            else: 
+            else:
                 print("damper not open yet")
 
             # if more supply is needed...
             if net_cfm > 0:
-                net_cfm -= await turnOnSupply(fan_12_inch, net_cfm)
+                net_cfm -= await turn_on_supply(fan_12_inch, net_cfm)
             else:
                 await fan_12_inch.node.turn_off()
 
@@ -268,10 +261,10 @@ async def balanceCFM(exhaust_fans, supply_fans, exhaust_cfm, supply_cfm):
             await fan_8_inch.node.turn_off()
 
     elif supply_cfm > 0:
-        await turnOffSupplies(damper, fan_12_inch, fan_8_inch)
+        await turn_off_supplies(damper, fan_12_inch, fan_8_inch)
 
     return net_cfm
-            
+
 
 if __name__ == "__main__":
 
@@ -296,5 +289,3 @@ if __name__ == "__main__":
         )
     except KeyboardInterrupt:
         _LOGGER.warning("KeyboardInterrupt received. Disconnecting!")
-
-
